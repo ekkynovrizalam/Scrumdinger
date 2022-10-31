ec84e7f92866d2ecc9c8161e2d83db8f74514a0f
Ekky Novriza Alam <ekkynovriza@gmail.com>
2022-10-31 22:45:24 +0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ekky Novriza Alam on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
